b8c373077f91df6207b7b7869a019ea509b95ac5
Braian Ledantes <braianledantes@outlook.com>
2021-10-26 00:28:37 -0300

    cambio de vista y add gitignore

--- app/src/main/java/com/wonder/appfirebase/AuthActivity.kt
+++ app/src/main/java/com/wonder/appfirebase/AuthActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
         Log.d("Hola", "Braian")
     }
 }
