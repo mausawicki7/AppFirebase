d430f128a9cb2a467c19753148b7779ed365fdd5
Braian Ledantes <braianledantes@outlook.com>
2021-10-25 22:54:24 -0300

    autenticacion firebase terminada

--- app/src/main/java/com/wonder/appfirebase/AuthActivity.kt
+++ app/src/main/java/com/wonder/appfirebase/AuthActivity.kt
@@ -1,13 +1,82 @@
 package com.wonder.appfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.wonder.appfirebase.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
-        Log.d("Hola", "Braian")
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.signUpButton.setOnClickListener {
+            signIn()
+        }
+
+        binding.logInButton.setOnClickListener {
+
+        }
+    }
+
+    private fun logIn() {
+        if (binding.editTextTextEmailAddress.text.isNotEmpty()
+            && binding.editTextTextPassword.text.isNotEmpty()) {
+            val email = binding.editTextTextEmailAddress.text.toString()
+            val pass = binding.editTextTextPassword.text.toString()
+
+            FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+        }
+    }
+
+    private fun signIn() {
+        if (binding.editTextTextEmailAddress.text.isNotEmpty()
+            && binding.editTextTextPassword.text.isNotEmpty()) {
+            val email = binding.editTextTextEmailAddress.text.toString()
+            val pass = binding.editTextTextPassword.text.toString()
+
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.apply {
+            setTitle("Error")
+            setMessage("Error autenticando el usuario")
+            setPositiveButton("Aceptar", null)
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
--- app/src/main/java/com/wonder/appfirebase/HomeActivity.kt
+++ app/src/main/java/com/wonder/appfirebase/HomeActivity.kt
@@ -2,10 +2,37 @@ package com.wonder.appfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.wonder.appfirebase.databinding.ActivityHomeBinding
+
+enum class ProviderType {
+    BASIC
+}
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bundle = intent.extras
+        val email = bundle?.getString("email") ?: ""
+        val provider = bundle?.getString("provider") ?: ""
+
+        setup(email, provider)
+    }
+
+    private fun setup(email: String, provider: String) {
+        binding.emailTextView.text = email
+        binding.providerTextView.text = provider
+
+        binding.logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
     }
 }
